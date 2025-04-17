39e88889b5ce2f1595c1b889540c97bd8040321a
weconnect-tobias <tobias@weconnect.se>
2025-04-17 10:20:42 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,9 +10,9 @@
 	<meta name="msapplication-TileImage" content="img/icon-144x144-1.png" />
 	<link rel="stylesheet" type="text/css" href="css/weconnect2024.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  </head>
   <body>
-
-<header>
+  
   <div class="header-container">
     <img src="img/WeConnect-Logo-White-350.png" alt="WeConnect Logo">
     <nav>
