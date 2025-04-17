3ff6293870f78d2812093cb81d4fae662d538a83
weconnect-tobias <tobias@weconnect.se>
2025-04-17 10:44:12 +0200

    0.28

--- index.php
+++ index.php
@@ -20,8 +20,8 @@
     <li>
     <a href="#">Tjänster 
     <svg class="icon-chevron" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" aria-hidden="true" focusable="false">
-  <path fill="currentColor" d="M96 192h128c17.7 0 26.6 21.5 14.1 34L174.1 290c-7.6 7.6-19.8 7.6-27.3 0L81.9 226c-12.5-12.5-3.6-34 14.1-34z"/>
-</svg>
+    <path fill="currentColor" d="M96 192h128c17.7 0 26.6 21.5 14.1 34L174.1 290c-7.6 7.6-19.8 7.6-27.3 0L81.9 226c-12.5-12.5-3.6-34 14.1-34z"/>
+  </svg>
     </a>
       <ul>
         <li><a href="https://weconnect.se/nis-nis2-iso27001-loggning/">NIS/NIS2 & ISO27001 loggning</a></li>
@@ -36,8 +36,8 @@
     <li>
       <a href="https://weconnect.se/kontakt/">Kontakt 
       <svg class="icon-chevron" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" aria-hidden="true" focusable="false">
-  <path fill="currentColor" d="M96 192h128c17.7 0 26.6 21.5 14.1 34L174.1 290c-7.6 7.6-19.8 7.6-27.3 0L81.9 226c-12.5-12.5-3.6-34 14.1-34z"/>
-</svg>
+    <path fill="currentColor" d="M96 192h128c17.7 0 26.6 21.5 14.1 34L174.1 290c-7.6 7.6-19.8 7.6-27.3 0L81.9 226c-12.5-12.5-3.6-34 14.1-34z"/>
+  </svg>
      </svg>
       </a>
       <ul>
