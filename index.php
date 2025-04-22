14dbe5f5de95c20f340b818771135edd43909c2e
weconnect-tobias <tobias@weconnect.se>
2025-04-22 08:42:53 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,23 +4,20 @@
   <head>
   <meta charset="UTF-8">
 	<title>Vad är min IP-adress? | Se din publika IP – WeConnect</title>
-  <meta name="description" content="Se din publika IP-adress direkt på ip.weconnect.se – ingen inloggning, inga cookies, bara din aktuella IPv4- eller IPv6-adress. Perfekt vid IT-support eller nätverksfelsökning.">	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<link rel="canonical" href="https://ip.weconnect.se/" />
-  <link rel="icon" href="img/icon-144x144-1.png" sizes="192x192" />
+  <meta name="description" content="Visa din publika IPv4- eller IPv6-adress direkt på ip.weconnect.se – inga cookies, ingen inloggning. Perfekt för felsökning eller support.">  <link rel="icon" href="img/icon-144x144-1.png" sizes="192x192" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="seobility" content="df95fd8fec0aef960e98c9575023bef2">
 	<link rel="apple-touch-icon" href="img/icon-144x144-1.png" />
 	<meta name="msapplication-TileImage" content="img/icon-144x144-1.png" />
 	<link rel="stylesheet" type="text/css" href="css/weconnect2024.css">
   <meta property="og:title" content="Vad är min IP-adress? | WeConnect IP-tjänst">
   <meta property="og:locale" content="sv_SE">
-  <meta property="og:description" content="Kolla din publika IP-adress snabbt och enkelt – ingen spårning, bara ren info. Användbar vid felsökning, support eller nätverksinställningar.">
-  <meta property="og:image" content="https://ip.weconnect.se/img/WeConnect-Logo-White-350.png">
+  <meta property="og:description" content="Visa din publika IPv4- eller IPv6-adress direkt på ip.weconnect.se – inga cookies, ingen inloggning. Perfekt för felsökning eller support.">  <meta property="og:image" content="https://ip.weconnect.se/img/WeConnect-Logo-White-350.png">
   <meta property="og:url" content="https://ip.weconnect.se">
   <meta property="og:type" content="website">
   <meta name="twitter:card" content="summary_large_image">
   <meta name="twitter:title" content="Vad är min IP-adress? | WeConnect IP-tjänst">
-  <meta name="twitter:description" content="Din publika IP-adress direkt i webbläsaren. Enkel, gratis och utan spårning.">
-  <meta name="twitter:image" content="https://ip.weconnect.se/img/WeConnect-Logo-White-350.png">
+  <meta name="twitter:description" content="Visa din publika IPv4- eller IPv6-adress direkt på ip.weconnect.se – inga cookies, ingen inloggning. Perfekt för felsökning eller support.">  <meta name="twitter:image" content="https://ip.weconnect.se/img/WeConnect-Logo-White-350.png">
 
   
   <!-- Structured data (Schema.org) -->
